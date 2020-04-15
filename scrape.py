2219e16b6bf9db70fff79819e81ae34ebee1f581
ElijahWilde <42654406+ElijahWilde@users.noreply.github.com>
2020-04-14 22:46:57 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,7 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 20:43:28 2020
+
+@author: emet
+"""
+
 import pandas as pd
 import re
+finalResult = []
 
-df = pd.read_csv(r'C:\Users\emet\Desktop\journal_articles_1.csv')
+# FORMATTING DATA==============================================================
+
+df = pd.read_csv(r'C:\Users\emet\Desktop\COMP\journal_articles_1.csv')
 
 noDoi = []
 dois =[]
@@ -14,33 +24,98 @@ for i in range(len(df)) :
     
     if (len(doi) > 0):
         dois.append("10." + doi[0])
-        noDoi.append("")
+        #noDoi.append("")
 
     else:
         dois.append("")
-        noDoi.append(string)
-        
-        
-        
-#"I'm totally a real web browser and not a web scraping tool. Trust me."
+        #noDoi.append(string)
+
+
+# MAKING THE SOUP==============================================================     
 import requests
-headers = requests.utils.default_headers()
-headers.update({ 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
-
-
-
 from bs4 import BeautifulSoup
 
-url = "https://dx.doi.org/" + dois[16]
-#https://dx.doi.org/10.1177/0049124192021002005
+# Set headers
+headers = requests.utils.default_headers()
 
-req = requests.get(url, headers)
-soup = BeautifulSoup(req.content, 'html.parser')
-print(soup.prettify())
+#soup0, soup1, soup2 = BeautifulSoup('<p>will replace these objects later</p>')
+soups = []
 
+user_agent_list = [
+   #Chrome
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    #Firefox
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
+]
 
-#This is how the div I am looking for appears when I inspect the website:
-#<a class="entryAuthor" href="/action/doSearch?target=default&amp;ContribAuthorStored=Browne%2C+Michael+W" aria-label="Open contributor information pop-up for Michael W. Browne"> Michael W. Browne</a>
+def searchString(tagType, searchTerm): #tag type can be "any"
+    myList = []
+    if tagType == "any":
+        for soup in soups:
+            myList.append(soup.find_all(text = re.compile(searchTerm)))
+    else:
+        for soup in soups:
+            myList.append(soup.find_all(tagType, text = re.compile(searchTerm)))
+    
+    return myList
 
-#But this line does not work:
-#soup.find_all("a" class_="entryAuthor")
+def searchClass(tagType, myClass):
+    myList = []
+    for soup in soups:
+        myList.append(soup.find_all(tagType, {'class':  myClass}))
+    return myList
+    
+# SEARCHING DATA===============================================================
+#from torrequest import TorRequest
+import random
+
+def whoPublishedThis(soup): 
+   if (len(searchClass(("div"),("SAGECopyright"))[0]) > 0):
+       return 'SAGE'
+    
+mysteryDois = []
+
+dois = ["https://dx.doi.org/"+doi if doi != "" else doi for doi in dois]
+
+sagePapers = []
+numSage = 0
+for doi in dois:
+    if doi != "":
+        headers.update({ 'User-Agent': random.choice(user_agent_list)}) #switch browsers to keep em guessing
+        req = requests.get(doi, headers)
+
+        soup0 = BeautifulSoup(req.content, 'xml')
+        #soup1 = BeautifulSoup(req.content, 'html.parser')
+        #soup2 = BeautifulSoup(req.content, 'lxml')
+        
+        soups = [soup0] #in case I need to add more parsers
+        
+        pub = whoPublishedThis(soup0)
+        
+        if (pub == 'SAGE'):
+            numSage += 1
+            sagePapers.append()
+        else: 
+            mysteryDois.append(doi)
+        
